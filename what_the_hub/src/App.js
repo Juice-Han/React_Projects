455386ffeaf8758b049c3f36f29f3b3a28e326d4
JuHan Park <iiiamst16@gmail.com>
2023-12-30 11:32:14 +0900

    study axios library

--- what_the_hub/src/App.js
+++ what_the_hub/src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [wiseSaying, setWiseSaying] = useState('')
+  async function getAdvice(){
+    const res = await axios.get('https://api.adviceslip.com/advice')
+    setWiseSaying(res.data['slip']['advice'])
+  }
   return (
-    <>
-    </>
+    <div className='app'>
+      <button onClick={getAdvice}>명언 가져와!</button>
+      <div className='wiseSaying-container'>
+        <div id='wiseSaying'>{wiseSaying}</div>
+      </div>
+    </div>
   );
 }
 
