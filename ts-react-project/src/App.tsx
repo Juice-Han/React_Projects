562381b59aaa2c41ebfe1dd94e257482a1601f02
JuHan Park <iiiamst16@gmail.com>
2023-12-31 21:19:38 +0900

    reset

--- ts-react-project/src/App.tsx
+++ ts-react-project/src/App.tsx
@@ -1,23 +1,13 @@
 import React, {useState} from 'react';
-import Library from './Library';
-import { Data } from './model/library';
+
+import axios from 'axios';
 import './App.css';
 
-let data = {
-  name : 'Juhan Library',
-  address : 'Incheon',
-  since : 2000,
-  books : [
-    {name : 'Harry Potter', status : 'O'},
-    {name : 'Peter Pan', status : 'X'},
-  ]
-}
-
 const App : React.FC = ()=> {
-
+  
   return (
     <div className="App">
-      <Library data={data}/>
+      
     </div>
   );
 }
