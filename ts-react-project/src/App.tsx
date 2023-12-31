3f9e38e46adfaf8ded3a4787c2bd005d4e1f139f
JuHan Park <iiiamst16@gmail.com>
2023-12-31 23:10:03 +0900

    add MenuBar Component

--- ts-react-project/src/App.tsx
+++ ts-react-project/src/App.tsx
@@ -1,15 +1,34 @@
 import React, {useState} from 'react';
-
 import axios from 'axios';
 import './App.css';
 
 const App : React.FC = ()=> {
-  
   return (
     <div className="App">
-      
+      <MenuBar/>
     </div>
   );
 }
 
+const MenuBar : React.FC = ()=> {
+  return(
+    <div className='menu-bar'>
+        <img className='insta-logo' src='img/menu-icon/insta-icon.png' alt=''/>
+        <div className='menu-icon-wrapper'>
+          <img src='img/menu-icon/home-icon.png' alt=''/>
+          <img src='img/menu-icon/search-icon.png' alt=''/>
+          <img src='img/menu-icon/compass-icon.png' alt=''/>
+          <img src='img/menu-icon/video-icon.png' alt=''/>
+          <img src='img/menu-icon/message-icon.png' alt=''/>
+          <img src='img/menu-icon/heart-icon.png' alt=''/>
+          <img src='img/menu-icon/plus-icon.png' alt=''/>
+          <img src='img/menu-icon/강아지.png' alt='' className='dog-icon'/>
+        </div>
+        <div className='bottom-menu-icon-wrapper'>
+          <img src='img/menu-icon/threads-icon.png' alt=''/>
+          <img src='img/menu-icon/threeline-icon.png'/>
+        </div>
+      </div>
+  )
+}
 export default App;
