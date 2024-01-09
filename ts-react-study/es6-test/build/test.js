4d77497c213282b566e9db0e918a11967d71ed4a
JuHan Park <iiiamst16@gmail.com>
2024-01-10 02:56:21 +0900

    finish es6-test

--- ts-react-study/es6-test/build/test.js
+++ ts-react-study/es6-test/build/test.js
@@ -1,4 +1,70 @@
 "use strict";
 
-var n = "John";
-console.log("Hello ".concat(n, "!!"));
+function _get() { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get.bind(); } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(arguments.length < 3 ? target : receiver); } return desc.value; }; } return _get.apply(this, arguments); }
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+var Person = /*#__PURE__*/function () {
+  function Person(name, tel, address) {
+    _classCallCheck(this, Person);
+    this.name = name;
+    this.tel = tel;
+    this.address = address;
+    if (Person.count) {
+      Person.count++;
+    } else {
+      Person.count = 1;
+    }
+  }
+  _createClass(Person, [{
+    key: "toString",
+    value: function toString() {
+      return "name=".concat(this.name, ", tel=").concat(this.tel, ", address=").concat(this.address);
+    }
+  }], [{
+    key: "getPersonCount",
+    value: function getPersonCount() {
+      return Person.count;
+    }
+  }]);
+  return Person;
+}();
+var Employee = /*#__PURE__*/function (_Person) {
+  _inherits(Employee, _Person);
+  var _super = _createSuper(Employee);
+  function Employee(name, tel, address, empno, dept) {
+    var _this;
+    _classCallCheck(this, Employee);
+    _this = _super.call(this, name, tel, address);
+    _this.empno = empno;
+    _this.dept = dept;
+    return _this;
+  }
+  _createClass(Employee, [{
+    key: "toString",
+    value: function toString() {
+      return _get(_getPrototypeOf(Employee.prototype), "toString", this).call(this) + ", empno=".concat(this.empno, ", dept=").concat(this.dept);
+    }
+  }, {
+    key: "getEmpInfo",
+    value: function getEmpInfo() {
+      return "".concat(this.empno, " : ").concat(this.name, "\uC740 ").concat(this.dept, " \uBD80\uC11C\uC785\uB2C8\uB2E4.");
+    }
+  }]);
+  return Employee;
+}(Person);
+var e1 = new Employee('이응룡', '010-2313-1234', '서울시', 'A12311', '회계팀');
+console.log(e1.getEmpInfo());
+console.log(e1.toString());
+console.log(Person.getPersonCount());
